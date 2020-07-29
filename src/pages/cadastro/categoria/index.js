0920750e1574aa565a1b3c70aa4d5d0f6f34d146
jonnyaccastro <joaoaccastro@gmail.com>
2020-07-29 18:13:09 -0300

    atualizacao slider

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -7,6 +7,18 @@ function cadastroCategoria(){
       <PageDefault>
         <h1>Cadastro de Categoria</h1>
 
+        <form>
+          <label>
+            Nome da Categoria:
+              <input
+                type="text"
+              />
+          </label>
+          <button>
+            Cadastrar
+          </button>
+        </form>
+
         <Link to="/"> {/* nesse caso como estamos usando o link ao inves de usarmos o href usamos o to */}
             Ir para home
         </Link>
