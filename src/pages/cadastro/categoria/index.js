eb57605428ca45caf23655430dddc55e5b2bb5e5
jonnyaccastro <joaoaccastro@gmail.com>
2020-07-29 21:55:20 -0300

    css cadastro de categoria

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';    
+/* import { Link } from 'react-router-dom';   */  
 import PageDefault from '../../../components/PageDefault';
 
 function cadastroCategoria(){
@@ -8,21 +8,32 @@ function cadastroCategoria(){
         <h1>Cadastro de Categoria</h1>
 
         <form>
-          <label>
-            Nome da Categoria:
-              <input
-                type="text"
-              />
-          </label>
+          <div class="box">
+            <label>
+              Nome:
+              <input type="text"/>
+            </label>
+            <br/>
+            <label>
+              Descrição:
+              <input type="text"/>
+            </label>
+            <br/>
+            <label>
+              Título do Vídeo:
+              <input type="text"/>
+            </label>
+          </div>
+          <br/>
           <button>
             Cadastrar
           </button>
         </form>
-
-        <Link to="/"> {/* nesse caso como estamos usando o link ao inves de usarmos o href usamos o to */}
+        {/*
+        <Link to="/"> nesse caso como estamos usando o link ao inves de usarmos o href usamos o to
             Ir para home
         </Link>
-
+        */}
       </PageDefault>
     )
 }
