f984ae1e3a5b2c1f3d3172c0c9ac7c0a5fbd7417
jonnyaccastro <joaoaccastro@gmail.com>
2020-08-15 12:51:52 -0300

    img-pag404

--- src/pages/Pagina404/index.js
+++ src/pages/Pagina404/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import PageDefault from '../../../src/components/PageDefault';
+import patienceImg from '../../assets/img/paciencia.png';
 
 function Pagina404(){
     return(
       <PageDefault>
         <div>
-            <h1>.:Página 404:.</h1>
-            Ops, ainda não tem nada por aqui, tente outro endereço ou tente novamente mais tarde!
+          <h1>.:Página 404:.</h1>
+          Ops, me desculpe, mas ainda não tem nada por aqui, tente outro endereço ou tente novamente mais tarde!
+        </div>
+        <br></br>
+        <div>
+          {/* eslint-disable-next-line */}
+          <img src={ patienceImg } width='200'/>
         </div>
       </PageDefault>
     )
 }
-
 export default Pagina404;
